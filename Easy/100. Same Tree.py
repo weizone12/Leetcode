bb7f772875535fbd071b279049dbead66c75526d
weizone12 <lovetw.plc@gmail.com>
2022-09-07 17:50:45 +0800

    first commit

---
+++ Easy/100. Same Tree.py
@@ -0,0 +1,12 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def isSameTree(p: TreeNode, q: TreeNode):
+    if not p and not q:
+        return True
+    if not p or not q or p.val != q.val:
+        return False
+    return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
